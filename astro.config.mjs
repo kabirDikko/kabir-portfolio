48b5badd0b82de9f80383b4b8985d386178687fb
Dayan Graham <d.graham50@hotmail.co.uk>
2025-03-12 21:21:01 -0500

    chore: Update astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ const isDev = process.env.NODE_ENV === "development";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
-  site: "https://kabirdikko.github.io",
-  base: isDev ? "/" : "/homesite",
+  site: "https://kabirDikko.github.io",
+  base: isDev ? "/" : "/kabir-portfolio",
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro",
